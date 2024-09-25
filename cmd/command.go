01b4180f067849b58ef65fc66080f4c257704d10
scorcism <abhishekpathak1720@gmail.com>
2024-09-25 18:54:49 +0530

    add source

---
+++ cmd/command.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CmdFlags struct {
+	Add    string
+	Delete int
+	Edit   string
+	Toggle int
+	List   bool
+}
+
+func NewCmdFlags() *CmdFlags {
+	cf := CmdFlags{}
+
+	flag.StringVar(&cf.Add, "add", "", "Add a new todo specified title")
+	flag.StringVar(&cf.Edit, "edit", "", "Edit todo by index and specify a new title. id:new_title")
+	flag.IntVar(&cf.Delete, "delete", -1, "Specifiy a todo by index to delete")
+	flag.IntVar(&cf.Toggle, "toggle", -1, "Specifiy a todo by index to toggle")
+	flag.BoolVar(&cf.List, "list", false, "List all todos")
+
+	flag.Parse()
+
+	return &cf
+}
+
+func (cf *CmdFlags) Execute(todos *Todos) {
+	switch {
+	case cf.List:
+		todos.print()
+	case cf.Add != "":
+		todos.add(cf.Add)
+	case cf.Edit != "":
+		parts := strings.SplitN(cf.Edit, ":", 2)
+		if len(parts) != 2 {
+
+			fmt.Println("Error, invalid format for edit. Please use id:new_title")
+			os.Exit(1)
+		}
+		index, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error: invalid index for edit")
+			os.Exit(1)
+		}
+		todos.edit(index, parts[1])
+	case cf.Toggle != -1:
+		todos.toggle(cf.Toggle)
+	case cf.Delete != -1:
+		todos.delete(cf.Delete)
+	default:
+		fmt.Println("Invalid command")
+	}
+}
